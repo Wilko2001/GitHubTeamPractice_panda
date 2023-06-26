ddff14b602d004e8ea3f15092a04723bd299f139
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-06-26 17:42:18 +0100

    installed all relevant packages and dev deps, updated styling for header

---
+++ surreal-estate/src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      Surreal Estate
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
